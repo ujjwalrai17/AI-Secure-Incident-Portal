0eb2c31631eb77ee42845f61e7b858bb19244e6a
ujjwal rai <ujjwalrai0429@gmail.com>
2026-09-11 18:55:15 +0530

    Project Setup

---
+++ src/main/java/com/ujjwal/aisecurity/AiSecureIncidentPortalApplication.java
@@ -0,0 +1,13 @@
+package com.ujjwal.aisecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSecureIncidentPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiSecureIncidentPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ujjwal/aisecurity/AiSecureIncidentPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ujjwal.aisecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSecureIncidentPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
