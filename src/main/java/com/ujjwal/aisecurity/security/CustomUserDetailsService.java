b3437a45a3250bf94901e15604b737ccec781a2a
ujjwal rai <ujjwalrai0429@gmail.com>
2026-09-26 10:00:12 +0530

    Implemented CustomUserDetailsService

---
+++ src/main/java/com/ujjwal/aisecurity/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+
+package com.ujjwal.aisecurity.security;
+
+import com.ujjwal.aisecurity.entity.User;
+import com.ujjwal.aisecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = repository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole()))
+        );
+    }
+}
